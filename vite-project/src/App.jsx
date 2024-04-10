f6a588a3d2a2026dc381f85159828e9cc25d03b2
JDRengifo <josedariorf@gmail.com>
2024-04-10 17:30:26 -0300

    imagen Magica

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import registro from './utils/registro.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='fotoRegistro'>
+      <img src={registro} alt="foto" className='fotoRam'/>
+    </div>
+     
+      
+  )
+}
+
+export default App
